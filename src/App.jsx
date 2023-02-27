fa3b8ab2e140bb82b0b4cd7a8e204023a5a71675
IsaacGC00 <A.IsaacGC@outlook.com>
2023-02-26 18:11:24 -0600

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
     <div className='font-quicksand'>
       <main className='bg-slate-100'>
         <section> 
-          <nav className='flex items-center justify-between text-slate-100 font-semibold text-xl bg-cyan-700 w-full h-12'>
+          <nav className='flex items-center justify-between py-1 text-slate-100 font-semibold text-xl bg-cyan-700 '>
             <h1 className={`${font_size.title}`}>Isaac Portfolio</h1>
-            <ul className="flex items-center gap-1 sm:gap-5 font-normal">
+            <ul className="flex items-center gap-2 sm:gap-2 font-normal">
               <li className={`${styles.hover_navbar} ${font_size.navbar}`}>Home</li>
               <li className={`${styles.hover_navbar} ${font_size.navbar}`}>About me</li>
               <li className={`${styles.hover_navbar} ${font_size.navbar}`}>Projects</li>
@@ -20,9 +20,10 @@ function App() {
             </ul>
           </nav>
         </section>
-        <section className='flex items-center justify-left'>
-          <img className=' w-60 h-60 border-cyan-700 border-2 object-cover overflow-hidden m-5 rounded-full' src="/src/assets/photo.jpg" alt="Image of Isaac"  />
-          <h1 className='text-2xl '>This is me</h1>
+        <section className='flex items-center justify-left mx-auto'>
+          <img className=' w-60 h-60 bg-cyan-700 p-1 object-cover overflow-hidden m-5 rounded-full' src="/src/assets/photo.jpg" alt="Image of Isaac"  />
+          <h1 className={`${font_size.h1}`}>Hi, I'm Isaac Guzman.</h1>
+          <h1 className={`${font_size.h1}`}>I'm a front-end developer</h1>
         </section>
       </main>
     </div>
--- src/style.js
+++ src/style.js
@@ -1,10 +1,11 @@
 const styles = {
-    hover_navbar: 'flex px-1 md:px-2 hover:bg-slate-100 hover:text-cyan-700 h-12 items-center cursor-pointer',
+    hover_navbar: 'hover:bg-slate-100 hover:text-cyan-700 cursor-pointer',
 };
 
 export const font_size = {
     title: "sm:text-2xl text-base px-1/2 sm:px-2",
     navbar: "sm:text-xl text-sm px-1/2 sm:px-2",
+    h1: "sm:text-3xl text-cyan-700 "
 };
 
 export default styles;
